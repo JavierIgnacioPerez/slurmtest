d247a34175e960c70d2a0a78200da55e66432402
Javier Pérez <javier.perez.ca@usach.cl>
2021-06-09 23:00:56 -0400

    Feat: Added first test script.

---
+++ test1.py
@@ -0,0 +1,33 @@
+# Gets a value from args for amplitude 
+# Amplitude is between 0.1 and 1. Need to be multiple of 0.1
+# This need validation.
+
+# This script runs a simulation iterating all the possible values
+# for ftrain (0.05kHz to 1 kHz) with a specific value for amplitude
+
+# Writes a file with the results in ./results/test1/...
+
+# Imports
+import os
+import numpy as np
+
+# Get data args from bash
+amp = 0.1
+
+# Creates output folder
+if not os.path.exists("results"):
+  os.mkdir("results")
+if not os.path.exists("results/test1"):
+  os.mkdir("results/test1")
+
+# Simulation sim
+ftrain = 0.05
+while ftrain <= 1:
+  # Creates aux strings.
+  amp_s = "amp_" + str(amp) + "_"
+  ftrain_s = "ftrain_" + str(ftrain)
+  # Creates file with garbage
+  np.savetxt("results/test1/" + amp_s + ftrain_s + ".txt", [amp,ftrain])
+  # Increments
+  ftrain += 0.01
+  ftrain = round(ftrain,2)
